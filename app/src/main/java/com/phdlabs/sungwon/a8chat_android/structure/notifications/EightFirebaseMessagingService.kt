5b6433fc0dbd4727351bd79763db20f7f5ce5899
Paixols <janayamuedano@fullsail.edu>
2018-03-19 11:06:22 -0700

    Adding Notifications

---
+++ app/src/main/java/com/phdlabs/sungwon/a8chat_android/structure/notifications/EightFirebaseMessagingService.kt
@@ -0,0 +1,9 @@
+package com.phdlabs.sungwon.a8chat_android.structure.notifications
+
+import com.google.firebase.messaging.FirebaseMessagingService
+
+/**
+ * Created by paix on 3/19/18.
+ */
+class EightFirebaseMessagingService: FirebaseMessagingService() {
+}
