29df6b554e0f1c1b036e83fd9f7ca0f55b6475d1
Paixols <janayamuedano@fullsail.edu>
2018-04-17 00:17:03 -0500

    UI for Terms & Agreement

---
+++ app/src/main/java/com/phdlabs/sungwon/a8chat_android/structure/login/DisclosureActivity.kt
@@ -0,0 +1,20 @@
+package com.phdlabs.sungwon.a8chat_android.structure.login
+
+import com.phdlabs.sungwon.a8chat_android.R
+import com.phdlabs.sungwon.a8chat_android.structure.core.CoreActivity
+
+/**
+ * Created by JPAM on 4/17/18.
+ * Privacy Policy
+ * Terms & Conditions
+ */
+class DisclosureActivity : CoreActivity() {
+    override fun layoutId(): Int = R.layout.activity_terms_conditions
+
+    override fun contentContainerId(): Int = 0
+
+    //TODO: Finish Disclosure activity after confirmation activity
+
+    //TODO: Set onActivity Result
+
+}
